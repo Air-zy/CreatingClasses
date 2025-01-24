1c2fd856cd0c47290e1a0bf703ad6b9b7122ad80
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-01-23 17:14:56 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
+using System;
 using People;
 
-Person person = new Person {
+Person ianBrooks = new Person {
     personId = 1,
     firstName = "Ian",
     lastName = "Brooks",
@@ -8,11 +9,42 @@ Person person = new Person {
     age = 30,
     isWorking = true
 };
+Person ginaJames = new Person {
+    personId = 2,
+    firstName = "Gina",
+    lastName = "James",
+    favoriteColour = "Green",
+    age = 18,
+    isWorking = false
+};
+Person mikeBriscoe = new Person {
+    personId = 3,
+    firstName = "Mike",
+    lastName = "Briscoe",
+    favoriteColour = "Blue",
+    age = 45,
+    isWorking = true
+};
+Person maryBeals = new Person {
+    personId = 4,
+    firstName = "Mary",
+    lastName = "Beals",
+    favoriteColour = "Yellow",
+    age = 30,
+    isWorking = true
+};
 
-person.DisplayPersonInfo();
-person.ChangeFavoriteColour();
+ginaJames.DisplayPersonInfo();
+Console.WriteLine(mikeBriscoe.ToString());
+ianBrooks.ChangeFavoriteColour();
+ianBrooks.DisplayPersonInfo();
 
-Console.WriteLine($"age in 10 years: {person.GetAgeInTenYears()}");
+Relation sisters = new Relation {
+    RelationshipType = "Sisterhood"
+};
+sisters.ShowRelationShip(ginaJames, maryBeals);
 
-Console.WriteLine(person.ToString());
-
+Relation brothers = new Relation {
+    RelationshipType = "Brotherhood"
+};
+brothers.ShowRelationShip(ianBrooks, mikeBriscoe);
